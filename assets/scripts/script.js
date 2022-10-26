8fb2a52b7223773ac2b76c73fa130fd32ed2ff0b
Younnes Chebli <younnesc@gmail.com>
2022-10-26 16:07:40 +0200

    fixed exo1

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,15 @@
 const brussels = document.getElementById("brussels");
 const now = new Date();
+console.log();
 brussels.innerText = now.toLocaleString();
 
 const anchorage = document.getElementById("anchorage");
 const anchorageTime = now.toLocaleString("fr-BE",{timeZone: "America/Anchorage"});
 anchorage.innerText = anchorageTime;
 
-// const reykjavik = document.getElementById("reykjavik");
-// const reykjavikTime = now.toLocaleString("fr-BE", {timeZone: "Atlantic/Ittoqqortoormiit"});
-// reykjavik.innerText = reykjavikTime;
+const reykjavik = document.getElementById("reykjavik");
+const reykjavikTime = now.toLocaleString("fr-BE", {timeZone: "Atlantic/Reykjavik"});
+reykjavik.innerText = reykjavikTime;
 
 const saintPetersburg = document.getElementById("saint-petersburg");
 const saintPetersburgTime = now.toLocaleString("fr-BE",{timeZone: "Europe/Moscow"});
