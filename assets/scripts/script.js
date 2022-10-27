f130a59dd1422e6c601c49afc53ceb3543d86a7f
Younnes Chebli <younnesc@gmail.com>
2022-10-27 10:08:57 +0200

    added exo3

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+////// 1
+
 const brussels = document.getElementById("brussels");
 const now = new Date();
 brussels.innerText = now.toLocaleString();
@@ -14,7 +16,7 @@ const saintPetersburg = document.getElementById("saint-petersburg");
 const saintPetersburgTime = now.toLocaleString("fr-BE",{timeZone: "Europe/Moscow"});
 saintPetersburg.innerText = saintPetersburgTime;
 
-//////
+////// 2
 
 const msToDays = (ms) => {
     return Math.floor(ms / (24*60*60*1000));
@@ -27,3 +29,42 @@ const birthdateToNow = nowInMs - birthdateInMs;
 const birthdateToNowInDays = msToDays(birthdateToNow);
 const birthdateToNowDisplay = document.getElementById("birthdateToNow");
 birthdateToNowDisplay.innerText = birthdateToNowInDays;
+
+////// 3
+
+const dateAndTimeDisplay = document.getElementById("date-after-display-container");
+let numberDisplay = document.getElementById("number-display");
+let dateDisplay = document.getElementById("date-after-display");
+const numberInput = document.getElementById("number-input");
+
+const displayDateAndTime = (h, date) => {
+    numberDisplay.innerText = h;
+    dateDisplay.innerText = date;
+    dateAndTimeDisplay.removeAttribute("hidden");
+};
+
+const hToMs = (h) => {
+    return Math.floor(h * (60*60*1000));
+};
+
+const numberHandling = (e) => {
+    let hValue;
+    if(e.target.value == "") {
+        hValue = 0;
+    } else {
+        hValue = e.target.value;
+    }
+    const msValue = hToMs(hValue);
+    const msfuture = now.getTime() + msValue;
+    let dateFuture = new Date(msfuture);
+    dateFuture = dateFuture.toLocaleString();
+    displayDateAndTime(hValue, dateFuture);
+};
+
+const init = () => {
+    dateAndTimeDisplay.setAttribute("hidden", "");
+}
+
+numberInput.addEventListener("input", numberHandling);
+numberInput.addEventListener("focusout", init);
+
